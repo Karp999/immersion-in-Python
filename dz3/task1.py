e4bc937fe3a9787b0510c98fecd6f74e50f9cebd
Anna.K <kkk.aanya1996@mail.ru>
2023-07-04 19:27:33 +0300

    Решение задачи про список с дублями

--- dz3/task1.py
+++ dz3/task1.py
@@ -2,3 +2,19 @@
 # Дан список повторяющихся элементов.
 # Вернуть список с дублирующимися элементами. 
 # В результирующем списке не должно быть дубликатов.
+
+print('')
+repeating_elements = [2, 9, 7, 3, 9, 7, 3, 8, 8, 8, 3, 7, 9, 3, 7, 9, 2]
+print(f'Исходный список повторяющихся элементов: {repeating_elements}')
+print('')
+
+no_duplicates = []
+
+for i in repeating_elements:
+    if repeating_elements.count(i) > 1 : 
+        no_duplicates.append(i)
+        
+no_duplicates = list(set(no_duplicates))
+
+print(f'Список повторяющихся элементов из исходного списка без дублей: {no_duplicates}')
+print('')
