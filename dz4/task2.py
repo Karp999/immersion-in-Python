b89cd0b56457507c41eb1d041cbf71ea563c098a
Anna.K <kkk.aanya1996@mail.ru>
2023-07-06 19:59:16 +0300

    задача про словарь

--- dz4/task2.py
+++ dz4/task2.py
@@ -2,3 +2,16 @@
 # Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь, 
 # где ключ — значение переданного аргумента, а значение — имя аргумента. Если
 # ключ не хешируем, используйте его строковое представление.
+
+
+def f_dict(**arguments):
+    new_dict = {}
+    for keys, values in arguments.items():
+        if isinstance(values, (list, dict, set, bytearray)):
+            values = str(values)
+        new_dict[values] = keys
+    return new_dict
+
+
+print(f_dict(genre = 'fairy tale', writers = {'Pushkin': 7, 'Andersen': 200}, publication_dates = ['1800-е',
+      '1810-е', '1820-е', '1830-е']))
