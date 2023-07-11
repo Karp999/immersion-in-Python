f8a7f61d73e69d93cfe3fcaf2dcca1d5b180aa74
Anna.K <kkk.aanya1996@mail.ru>
2023-07-11 21:56:59 +0300

    Задача первая - разделение абсолютного пути файла на кортеж из трёх элементов

--- dz5/task1.py
+++ dz5/task1.py
@@ -1,3 +1,13 @@
 # Задача №1: 
 # Напишите функцию, которая принимает на вход строку — абсолютный путь до файла. 
 # Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+
+def file_extension(path: str) -> tuple:
+    path, name = path.rsplit('\\', 1)
+    name, extension = name.split('.')
+    return path, name, extension
+
+print()
+separation_extension = r'C:\Users\79995\Desktop\GEEK BRAINS\6.seminars\2 semester 2023\python2\hm\dz5\cat.jpg'
+print(file_extension(separation_extension))
+print()
