42fb61f749ea0065c0e2817e4aaf1635bfa1a73a
Anna.K <kkk.aanya1996@mail.ru>
2023-07-11 22:22:58 +0300

    Задача про числа Фибоначчи

--- dz5/task3.py
+++ dz5/task3.py
@@ -1,2 +1,14 @@
 # Задача №3:
 # Создайте функцию генератор чисел Фибоначчи (см. Википедию).
+
+def Fibonacci_nums(n: int) -> list[int]:
+    a, b = 0, 1
+    for __ in range(n):
+        yield a
+        a, b = b, a + b
+
+print()
+print('Числа Фибоначчи: ')
+print()
+print(*(Fibonacci_nums(20)))
+print()
